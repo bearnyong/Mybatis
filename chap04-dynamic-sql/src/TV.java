946b09002092bf2acc00fcf96495140cc0bbba57
bearnyong <rhalsdud216@gmail.com>
2023-06-16 23:33:32 +0900

    mybatis_finish

---
+++ chap04-dynamic-sql/src/TV.java
@@ -0,0 +1,20 @@
+
+public class TV {
+	String color; /*기능 : 색상*/
+	boolean power; /*기능 : 전원 상태*/
+	int channel; /*기능 : 채널*/
+	
+	/*속성 : 켜기,끄기*/
+	void power() {
+	}
+	
+	/*속성 : 채널 변경하기(↑)*/
+	void channelUp() {	
+	}
+	
+	/*속성 : 채널 변경하기(↓)*/
+	void channelDown() {
+	}
+	
+	
+}
---
+++ chap04-dynamic-sql/src/test.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class test {
+	
+	/*static 초기화*/
+	static int[] arr = new int[10];
+	
+	static {
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = (int)(Math.random()*10)+1;
+		}
+	}
+}
